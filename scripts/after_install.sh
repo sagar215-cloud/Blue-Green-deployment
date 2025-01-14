9e21731f0d8bbf4924447cdef8fea3ef155dc5fb
sagar215-cloud <vidyasagardevops9@gmail.com>
2025-01-15 00:20:27 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set -e
+
+sudo mkdir -p /var/lock/apache2
+sudo chown -R www-data:www-data /var/lock/apache2
+
 echo "Running AfterInstall hook..."
 sudo chmod -R 755 /var/www/html
+sudo chmod -R 755 /etc/apache2
