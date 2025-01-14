c930338ee3a53a2965177b8b4f9c446434fcd778
sagar215-cloud <vidyasagardevops9@gmail.com>
2025-01-15 00:18:36 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,18 +1,6 @@
 #!/bin/bash
 set -e
 
-sudo mkdir -p /var/cache/apt/archives/partial
-sudo chown -R _apt:root /var/cache/apt/archives
-
-sudo rm -rf /var/lib/dpkg/lock-frontend
-sudo rm -rf /var/lib/dpkg/lock-frontend
-
-# Webserver install and configure.
-sudo apt update -y
-
-# Install Apache web server
-sudo apt install -y apache2
-
 # Start and enable the apache2 service
 sudo systemctl start apache2
 sudo systemctl enable apache2
